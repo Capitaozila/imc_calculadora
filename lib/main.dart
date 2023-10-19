1b313065c1f094d7d11c668c0521c9772028474d
Capitaozila <estudanteluis3030@gmail.com>
2023-10-19 18:41:31 -0300

    adicionadouma lista de IMCs passados

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,20 @@ void main() {
 }
 
 class ImcCalculadora extends StatelessWidget {
-  const ImcCalculadora({super.key});
+  const ImcCalculadora({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF171717),
+        primaryColor: const Color(0xFF2C3E50),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Calculadora de IMC"),
+          backgroundColor: const Color(0xFF2C3E50),
         ),
         body: const CalculatorScreen(),
       ),
@@ -22,7 +27,7 @@ class ImcCalculadora extends StatelessWidget {
 }
 
 class CalculatorScreen extends StatefulWidget {
-  const CalculatorScreen({super.key});
+  const CalculatorScreen({Key? key});
 
   @override
   createState() => _CalculatorScreenState();
@@ -34,6 +39,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   double _peso = 0.0;
   double _imcResultado = 0.0;
   String _classificacao = '';
+  final List<double> _listaImc = [];
 
   double calcularIMC(double altura, double peso) {
     double imc = peso / ((altura / 100) * (altura / 100));
@@ -60,48 +66,85 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     }
   }
 
+  final shadowStyle = BoxShadow(
+    color: Colors.black.withOpacity(0.5),
+    spreadRadius: 3,
+    blurRadius: 5,
+    offset: const Offset(0, 3),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView( // Use SingleChildScrollView para permitir a rolagem
+    return SingleChildScrollView(
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
               'Informe seus dados',
-              style: TextStyle(fontSize: 20.0),
+              style: TextStyle(
+                  fontSize: 20.0, color: Color.fromARGB(255, 255, 255, 255)),
             ),
             const SizedBox(height: 20.0),
-            TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  _nome = value;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Nome',
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [shadowStyle],
+              ),
+              child: TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    _nome = value;
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Nome',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                ),
               ),
             ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _altura = double.tryParse(value) ?? 0.0;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Altura (cm)',
+            const SizedBox(height: 10.0),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [shadowStyle],
+              ),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _altura = double.tryParse(value) ?? 0.0;
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Altura (cm)',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                ),
               ),
             ),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  _peso = double.tryParse(value) ?? 0.0;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Peso (kg)',
+            const SizedBox(height: 10.0),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [shadowStyle],
+              ),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _peso = double.tryParse(value) ?? 0.0;
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Peso (kg)',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                ),
               ),
             ),
             const SizedBox(height: 20.0),
@@ -110,18 +153,47 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                 setState(() {
                   _imcResultado = calcularIMC(_altura, _peso);
                   _classificacao = classificarIMC(_imcResultado);
+                  _listaImc.add(_imcResultado);
                 });
               },
-              child: const Text('Calcular IMC'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 255, 255, 255),
+              ),
+              child: const Text(
+                'Calcular IMC',
+                style: TextStyle(color: Colors.black),
+              ),
             ),
             const SizedBox(height: 20.0),
             Text(
               'Olá $_nome! Seu IMC é: $_imcResultado',
-              style: const TextStyle(fontSize: 20.0),
+              style: const TextStyle(
+                  fontSize: 20.0, color: Color.fromARGB(255, 255, 255, 255)),
             ),
             Text(
               'Classificação: $_classificacao',
-              style: const TextStyle(fontSize: 20.0),
+              style: const TextStyle(
+                  fontSize: 20.0, color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [shadowStyle],
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _listaImc.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        'IMC ${index + 1}: ${_listaImc[index]} um IMC ${classificarIMC(_listaImc[index])}!',
+                        style: const TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255))),
+                  );
+                },
+              ),
             ),
           ],
         ),
